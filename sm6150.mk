e4abc8941867a2155b303d68fa796ba13f3a885e
daredevil-07 <dheereshantony6@gmail.com>
2020-08-29 12:43:47 +0000

    ZenX-ify

--- sm6150.mk
+++ sm6150.mk
@@ -7,12 +7,15 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+#include vendor/oplauncher/OPLauncher2.mk 
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
+include vendor/oplauncher/OPLauncher2.mk 
 
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/sm6150-common/sm6150-common-vendor.mk)
